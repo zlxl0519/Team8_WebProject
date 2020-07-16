483162d695cfea524ef6597661947b0b91664dbf
seunghui <zlxl0519@naver.com>
2020-07-16 19:36:08 +0900

    reserveDto, reserveDao 변경

--- Team8_WebProject/src/test/reserve/dao/ReserveDao.java
+++ Team8_WebProject/src/test/reserve/dao/ReserveDao.java
@@ -2,6 +2,7 @@ package test.reserve.dao;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import test.reserve.dto.ReserveDto;
@@ -27,10 +28,14 @@ public class ReserveDao {
 			conn = new DbcpBean().getConn();
 			//sql 문 작성
 			String sql = "insert into reserve"
-					+ " (num, )";
+					+ " (num, service, checkin, checkout, etc)"
+					+ "	values(reserve_seq.nextval, ?, to_date('?', 'yyyy-mm-dd'), to_date('?', 'yyyy-mm-dd'), ?)";
 			pstmt = conn.prepareStatement(sql);
 			//sql 문에 ? 부분을 넣는것
-
+			pstmt.setString(1, dto.getService());
+			pstmt.setString(2, dto.getCheckin());
+			pstmt.setString(3, dto.getCheckout());
+			pstmt.setString(4, dto.getEtc());
 			flag = pstmt.executeUpdate();
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -49,5 +54,49 @@ public class ReserveDao {
 		} else {
 			return false;
 		}
+	}//예약 정보 추가하기 종료
+	
+	//한명의 예약정보 불러오는 메소드
+	public ReserveDto getData(int num) {
+		ReserveDto dto=null;
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		try {
+			conn = new DbcpBean().getConn();
+			String sql = "select m.name, m.phone, r.service, "
+					+ "	d.breed, d.dage,r.checkin,r.checkout, r.etc"
+					+ "	from am_member m, reserve r, am_dogs d"
+					+ "	where m.id=r.member_id and r.dog_num=d.num and r.num=? ";
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setInt(1, num);
+			rs = pstmt.executeQuery();
+			if (rs.next()) {
+				dto=new ReserveDto();
+				dto.setMember_name(rs.getString("m.name"));
+				dto.setMember_phone(rs.getString("m.phone"));
+				dto.setService(rs.getString("r.service"));
+				dto.setDog_breed(rs.getString("d.breed"));
+				dto.setDog_age(rs.getString("d.dage"));
+				dto.setCheckin(rs.getString("r.checkin"));
+				dto.setCheckout(rs.getString("r.checkout"));
+				dto.setEtc(rs.getString("r.etc"));
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (rs != null)
+					rs.close();
+				if (pstmt != null)
+					pstmt.close();
+				if (conn != null)
+					conn.close();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		return dto;
 	}
+	 
 }
--- Team8_WebProject/src/test/reserve/dto/ReserveDto.java
+++ Team8_WebProject/src/test/reserve/dto/ReserveDto.java
@@ -3,7 +3,11 @@ package test.reserve.dto;
 public class ReserveDto {
 	private int num;
 	private String member_id;
+	private String member_name;
+	private String member_phone;
 	private int dog_num;
+	private String dog_breed;
+	private String dog_age;
 	private String service;
 	private String checkin;
 	private String checkout;
@@ -11,12 +15,16 @@ public class ReserveDto {
 	
 	public ReserveDto() {}
 
-	public ReserveDto(int num, String member_id, int dog_num, String service, String checkin, String checkout,
-			String etc) {
+	public ReserveDto(int num, String member_id, String member_name, String member_phone, int dog_num, String dog_breed,
+			String dog_age, String service, String checkin, String checkout, String etc) {
 		super();
 		this.num = num;
 		this.member_id = member_id;
+		this.member_name = member_name;
+		this.member_phone = member_phone;
 		this.dog_num = dog_num;
+		this.dog_breed = dog_breed;
+		this.dog_age = dog_age;
 		this.service = service;
 		this.checkin = checkin;
 		this.checkout = checkout;
@@ -39,12 +47,44 @@ public class ReserveDto {
 		this.member_id = member_id;
 	}
 
+	public String getMember_name() {
+		return member_name;
+	}
+
+	public void setMember_name(String member_name) {
+		this.member_name = member_name;
+	}
+
+	public String getMember_phone() {
+		return member_phone;
+	}
+
+	public void setMember_phone(String member_phone) {
+		this.member_phone = member_phone;
+	}
+
 	public int getDog_num() {
 		return dog_num;
 	}
 
 	public void setDog_num(int dog_num) {
 		this.dog_num = dog_num;
+	}
+
+	public String getDog_breed() {
+		return dog_breed;
+	}
+
+	public void setDog_breed(String dog_breed) {
+		this.dog_breed = dog_breed;
+	}
+
+	public String getDog_age() {
+		return dog_age;
+	}
+
+	public void setDog_age(String dog_age) {
+		this.dog_age = dog_age;
 	}
 
 	public String getService() {
@@ -79,7 +119,5 @@ public class ReserveDto {
 		this.etc = etc;
 	}
 
-	
-	
 	
 }
