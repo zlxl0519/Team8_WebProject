11128b5a3f107ba9508b757f59f100089b1f59da
seunghui <zlxl0519@naver.com>
2020-07-27 16:37:14 +0900

    테이블 들 제약조건명 달고 제약조건 수정

--- Team8_WebProject/WebContent/WEB-INF/am_members.sql
+++ Team8_WebProject/WebContent/WEB-INF/am_members.sql
@@ -1,8 +1,13 @@
 create table am_member(
-id varchar2(35) primary key, 
-pwd varchar2(20), 
-email varchar2(40), 
+id varchar2(35) not null, 
+pwd varchar2(20) not null, 
+email varchar2(40) , 
 profile varchar2(100), 
-name varchar2(15), 
-phone varchar2(15), 
-regdate date);
+name varchar2(15) not null, 
+phone varchar2(15) not null, 
+regdate date,
+constraint am_member_id_pk primary key(id),
+
+);
+
+create sequence am_member_seq;
